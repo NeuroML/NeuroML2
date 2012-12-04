bc0545afb6baeb06906c272a4d7b0e1409ff8e4f
Padraig Gleeson <p.gleeson@gmail.com>
2012-12-04 11:09:25 +0000

    Adding new Schemas/NeuroML2/NeuroML_v2beta.xsd, to distinguish the beta release & some file examples/NML2_* valid according to it

--- test.py
+++ test.py
@@ -6,24 +6,46 @@ import os
 from lxml import etree
 from urllib import urlopen
 
-def_dir="NeuroML2CoreTypes"
-dirList=os.listdir(def_dir)
+lems_def_dir="NeuroML2CoreTypes"
+lems_def_list=os.listdir(lems_def_dir)
+
+nml2_ex_dir="examples"
+nml2_ex_list=os.listdir(nml2_ex_dir)
 
 lems_schema = "Schemas/LEMS/LEMS_v0.6.xsd"
 lems_schema_file = urlopen(lems_schema)
 
-xmlschema_doc = etree.parse(lems_schema_file)
-xmlschema = etree.XMLSchema(xmlschema_doc)
+lems_xmlschema_doc = etree.parse(lems_schema_file)
+lems_xmlschema = etree.XMLSchema(lems_xmlschema_doc)
 
-for file in dirList:
+nml2_schema = "Schemas/NeuroML2/NeuroML_v2beta.xsd"
+nml2_schema_file = urlopen(nml2_schema)
+
+nml2_xmlschema_doc = etree.parse(nml2_schema_file)
+nml2_xmlschema = etree.XMLSchema(nml2_xmlschema_doc)
+
+for file in lems_def_list:
     
-
     if file.endswith("xml") and not file.startswith("LEMS") and not file.startswith("Ex"):
 
         print "Validating %s against %s" %(file, lems_schema)
 
-        doc = etree.parse(def_dir+"/"+file)
-        valid = xmlschema.validate(doc)
+        doc = etree.parse(lems_def_dir+"/"+file)
+        valid = lems_xmlschema.validate(doc)
+        if valid:
+            print "  It is valid!"
+        else:
+            print "  *** It's NOT valid! ***"
+
+
+for file in nml2_ex_list:
+
+    if file.endswith("nml"):
+
+        print "Validating %s against %s" %(file, nml2_schema)
+
+        doc = etree.parse(nml2_ex_dir+"/"+file)
+        valid = nml2_xmlschema.validate(doc)
         if valid:
             print "  It is valid!"
         else:
