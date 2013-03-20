8bcdfc8a81997482cd5987e7b233329003b61d8e
Padraig Gleeson <p.gleeson@gmail.com>
2013-03-20 14:21:51 +0000

    Slightly improved getNeuroML

--- getNeuroML.py
+++ getNeuroML.py
@@ -3,13 +3,6 @@ import os
 import os.path as op
 import subprocess
 
-'''
-try:
-    from fabri3c.api import *
-except ImportError as e:
-    print "Problem loading Python module for Fabric"
-'''
-
 neuroml2_spec_repo = ['NeuroML/NeuroML2']
 libneuroml_repo = ['NeuralEnsemble/libNeuroML']
 
@@ -20,19 +13,18 @@ java_neuroml_repos = ['NeuroML/org.neuroml.model.injectingplugin',
                       'NeuroML/org.neuroml.import',
                       'NeuroML/jNeuroML']
 
-
 neuroml_repos = neuroml2_spec_repo + libneuroml_repo + java_neuroml_repos
 
 jlems_repo = ['LEMS/jLEMS']
 lems_spec_repos = ['LEMS/LEMS']
 pylems_repos = ['LEMS/pylems']
 
-java_repos = java_neuroml_repos + jlems_repo
+java_repos = jlems_repo + java_neuroml_repos
 lems_repos = jlems_repo + lems_spec_repos + pylems_repos
 
 all_repos = neuroml_repos + lems_repos
 
-# Set the prteferred method for cloning from GitHub
+# Set the preferred method for cloning from GitHub
 github_pref = "HTTP"
 #github_pref = "SSH"
 #github_pref = "Git Read-Only"
@@ -44,7 +36,7 @@ pre_gh["Git Read-Only"]="git://github.com/"
 
 
 def execute_command_in_dir(command, directory):
-    print ">> Executing: (%s) in dir: %s"%(command, directory)
+    print ">>>  Executing: (%s) in dir: %s"%(command, directory)
     return_string = subprocess.check_output("cd %s; %s"%(directory, command), shell=True)
     return return_string
 
@@ -67,7 +59,7 @@ for repo in all_repos:
 
     if repo in java_repos and runMvnInstall:
         command = "mvn install"
-        print "It's a Java repository, so installing using Maven"
+        print "It's a Java repository, so installing using Maven..."
         info = execute_command_in_dir(command, local_dir)
         if "BUILD SUCCESS" in info:
             print "Successful installation using : %s!"%command
@@ -76,12 +68,14 @@ for repo in all_repos:
             print info
             exit(1)
 
-print "All repositories successfully updated & Java modules built!"
-
 print
-print "You should be able to run some examples straight away using: "
+print "All repositories successfully updated & Java modules built!"
+print
+print "You should be able to run some examples straight away using jnml: "
 print
 print "  cd ../jNeuroML"
+print "  ./jnml -validate ../NeuroML2/examples/NML2_FullNeuroML.nml"
+print
 print "  ./jnml ../NeuroML2/NeuroML2CoreTypes/LEMS_NML2_Ex8_AdEx.xml"
 print
 
