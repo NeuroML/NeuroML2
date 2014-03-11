f6cb9289bd04dcf4586722471abd00c1dfe915fa
Padraig Gleeson <p.gleeson@gmail.com>
2014-03-11 12:22:11 +0000

    Initial version of v2beta3 schema

--- test.py
+++ test.py
@@ -42,7 +42,7 @@ lems_xmlschema = etree.XMLSchema(lems_xmlschema_doc)
 lems_ex_dir="../LEMS/examples"
 lems_ex_list=os.listdir(lems_ex_dir)
 
-nml2_schema_name = "NeuroML_v2beta2.xsd"
+nml2_schema_name = "NeuroML_v2beta3.xsd"
 nml2_schema = "Schemas/NeuroML2/%s"%nml2_schema_name
 nml2_schema_file = urlopen(nml2_schema)
 
